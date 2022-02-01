05a4ca378e5bf7be58e72026421eec53574ea68b
Leonid <bychkov.l47@mail.ru>
2022-02-01 13:31:18 +0300

    add files

---
+++ about-company.php
@@ -0,0 +1,129 @@
+<?php require("header.html"); ?>
+
+<?php require "breadcrumbs.html"; ?>
+
+<section class="about-company-text-image">
+    <img src="" alt="фотография здания tsbnk">
+
+    <div class="about-company-text">
+        <h1 class="heading-1">О Coins.tsbnk</h1>
+
+        <p class="about-company-main-text main-text">
+            COINS. TSBNK - часть вашей счастливой истории! Интернет-магазин Coins.tsbnk создан командой
+            профессиональных специалистов АКБ «Трансстройбанк» (АО), работающих на рынке драгоценных металлов более 8
+            лет. Стремительное развитие инвестиционного рынка и мировых технологий позволило нам создать эффективный
+            ресурс, предлагающий полный спектр услуг от выбора оригинального подарка до создания собственного
+            инвестиционного портфеля из драгоценных металлов. В нашем магазине представлены популярные российские и
+            иностранные монеты и редкие нумизматические экземпляры.
+        </p>
+    </div>
+</section>
+
+<section class="about-company-advantages">
+    <div class="advantage-item">
+        <div class="advantage-item-index">
+            <span class="heading-1 advantage-item-number">01</span>
+        </div>
+
+        <p class="advantage-item-text">
+            Наши сотрудники проведут грамотную консультацию и помогут в выборе лучшего варианта, исходя из
+            индивидуальных потребностей каждого клиента.
+        </p>
+    </div>
+
+    <div class="advantage-item">
+        <div class="advantage-item-index">
+            <span class="heading-1 advantage-item-number">02</span>
+        </div>
+
+        <p class="advantage-item-text">
+            Качество и подлинность монет гарантируется строгим контролем при приемке и высоким профессиональным
+            уровнем нашей команды.
+        </p>
+    </div>
+</section>
+
+<section class="clients-terms">
+    <div class="clients-up">
+        <p class="clients-text main-text">
+            Мы ценим время и сбережения своих клиентов, предлагая лучшие цены на рынке, расширяя ассортиментную
+            линейку и совершенствуя сервис доставки и заказа.<br><br>
+            Постоянным клиентам мы предлагаем гибкую систему скидок.
+        </p>
+
+        <img src="" alt="" />
+    </div>
+    
+    <p class="clients-down main-text">
+        В разделах <a href="#">Энциклопедия</a> и <a href="#">Новости</a> Вас ждут интересные исторические факты,
+        научные статьи, новинки в области рынка драгоценных металлов, различные аналитические материалы и обзоры.
+    </p>
+</section>
+
+<section class="news">
+    <h2 class="heading-2">Новости</h2>
+
+    <div class="news-list">
+        <div class="news-item">
+            <span class="news-item description-text">02 ноября</span>
+
+            <p class="news-item__announce-text main-text">
+                Шесть памятных монет выпущенных банком России получили награды XV международного конкурса памятных
+                монет “Монетное созвездие - 2021”
+            </p>
+        </div>
+
+        <div class="news-item">
+            <span class="news-item description-text">02 ноября</span>
+
+            <p class="news-item__announce-text main-text">
+                Шесть памятных монет выпущенных банком России получили награды XV международного конкурса памятных
+                монет “Монетное созвездие - 2021”
+            </p>
+        </div>
+
+        <div class="news-item">
+            <span class="news-item description-text">02 ноября</span>
+
+            <p class="news-item__announce-text main-text">
+                Шесть памятных монет выпущенных банком России получили награды XV международного конкурса памятных
+                монет “Монетное созвездие - 2021”
+            </p>
+        </div>
+    </div>
+</section>
+
+<section class="encyclopedia">
+    <h2 class="heading-2">Энциклопедия</h2>
+
+    <div class="encyclopedia-list">
+        <div class="encyclopedia-item">
+            <span class="encyclopedia-item__date description-text">02 ноября</span>
+
+            <p class="encyclopedia-item__announce-text main-text">
+                Шесть памятных монет выпущенных банком России получили награды XV международного конкурса памятных
+                монет “Монетное созвездие - 2021”
+            </p>
+        </div>
+
+        <div class="encyclopedia-item">
+            <span class="encyclopedia-item__date description-text">02 ноября</span>
+
+            <p class="encyclopedia-item__announce-text main-text">
+                Шесть памятных монет выпущенных банком России получили награды XV международного конкурса памятных
+                монет “Монетное созвездие - 2021”
+            </p>
+        </div>
+
+        <div class="encyclopedia-item">
+            <span class="encyclopedia-item__date description-text">02 ноября</span>
+
+            <p class="encyclopedia-item__announce-text main-text">
+                Шесть памятных монет выпущенных банком России получили награды XV международного конкурса памятных
+                монет “Монетное созвездие - 2021”
+            </p>
+        </div>
+    </div>
+</section>
+
+<?php require "footer.html";?>
---
+++ contacts.php
@@ -0,0 +1,256 @@
+<?php require("header.html"); ?>
+
+<?php require "breadcrumbs-white.html"; ?>
+
+<section class="contacts-top">
+    <h1 class="heading-1">Контакты</h1>
+
+    <div class="contacts-3-row">
+        <div class="contacts-row-item contacts">
+            <h2 class="heading-4 contacts-row-item__name">Контакты</h2>
+
+            <div class="contacts-row-item__email-tel">
+                <a href="tel:+7 (808) 505-04-76">+7 (808) 505-04-76</a>
+                <a href="mailto:coins.shop@tsbnk.ru">coins.shop@tsbnk.ru</a>
+            </div>
+
+            <div class="contacts-row-item__socials">
+                <a href="https://facebook.com/" target="_blank">
+                    <img src="/images/icons/facebook.svg" alt="facebook icon">
+                </a>
+
+                <a href="https://vk.com/" target="_blank">
+                    <img src="/images/icons/vk.svg" alt="vk icon">
+                </a>
+
+                <a href="https://ok.ru" target="_blank">
+                    <img src="/images/icons/odnoklassniki.svg" alt="одноклассники icon">
+                </a>
+            </div>
+        </div>
+
+        <div class="contacts-row-item opened-time">
+            <h2 class="heading-4 contacts-row-item__name">Режим работы</h2>
+            
+            <p class="main-text">
+                Заказы в интернете-магазине подтверждаются с Пн-Пт с 09.00 до 18.00
+            </p>
+        </div>
+
+        <div class="contacts-row-item optional">
+            <p class="heading-4 contacts-row-item__name">Уважаемые покупатели!</p>
+
+            <p class="main-text">
+                Магазин не работает с 27 декабря 2021 по 9 января 2022. Просьба приобрести интересующие вас монеты
+                заранее.
+            </p>
+        </div>
+    </div>
+</section>
+
+<section class="contacts-ymaps">
+    <div class="custom-ymaps-wrapper">
+
+    </div>
+</section>
+
+<section class="addresses">
+    <div class="inner-addresses-wrapper">
+        <div class="addresses-list">
+            <div class="address-item">
+                <address class="address-item__value heading-4">
+                    г. Москва, ул. Дубининская, д. 94 (Головной офис)
+                </address>
+
+                <div class="address-item__metro-stations">
+                    <span class="address-item__metro-station main-text">Павелецкая</span>
+                    <span class="address-item__metro-station main-text">Тульская</span>
+                </div>
+
+                <p class="address-item__opened-time description-text">Понедельник - пятница 09:30-17:00</p>
+
+                <a href="tel:+7 (800) 505 04 76" class="address-item__phone description-text">+7 (800) 505 04 76 </a>
+            </div>
+
+            <div class="address-item">
+                <address class="address-item__value heading-4">
+                    г. Москва, ул. Дубининская, д. 94 (Головной офис)
+                </address>
+
+                <div class="address-item__metro-stations">
+                    <span class="address-item__metro-station main-text">Павелецкая</span>
+                    <span class="address-item__metro-station main-text">Тульская</span>
+                </div>
+
+                <p class="address-item__opened-time description-text">Понедельник - пятница 09:30-17:00</p>
+
+                <a href="tel:+7 (800) 505 04 76" class="address-item__phone description-text">+7 (800) 505 04 76 </a>
+            </div>
+
+            <div class="address-item">
+                <address class="address-item__value heading-4">
+                    г. Москва, ул. Дубининская, д. 94 (Головной офис)
+                </address>
+
+                <div class="address-item__metro-stations">
+                    <span class="address-item__metro-station main-text">Павелецкая</span>
+                    <span class="address-item__metro-station main-text">Тульская</span>
+                </div>
+
+                <p class="address-item__opened-time description-text">Понедельник - пятница 09:30-17:00</p>
+
+                <a href="tel:+7 (800) 505 04 76" class="address-item__phone description-text">+7 (800) 505 04 76 </a>
+            </div>
+
+            <div class="address-item">
+                <address class="address-item__value heading-4">
+                    г. Москва, ул. Дубининская, д. 94 (Головной офис)
+                </address>
+
+                <div class="address-item__metro-stations">
+                    <span class="address-item__metro-station main-text">Павелецкая</span>
+                    <span class="address-item__metro-station main-text">Тульская</span>
+                </div>
+
+                <p class="address-item__opened-time description-text">Понедельник - пятница 09:30-17:00</p>
+
+                <a href="tel:+7 (800) 505 04 76" class="address-item__phone description-text">+7 (800) 505 04 76 </a>
+            </div>
+
+            <div class="address-item">
+                <address class="address-item__value heading-4">
+                    г. Москва, ул. Дубининская, д. 94 (Головной офис)
+                </address>
+
+                <div class="address-item__metro-stations">
+                    <span class="address-item__metro-station main-text">Павелецкая</span>
+                    <span class="address-item__metro-station main-text">Тульская</span>
+                </div>
+
+                <p class="address-item__opened-time description-text">Понедельник - пятница 09:30-17:00</p>
+
+                <a href="tel:+7 (800) 505 04 76" class="address-item__phone description-text">+7 (800) 505 04 76 </a>
+            </div>
+
+            <div class="address-item">
+                <address class="address-item__value heading-4">
+                    г. Москва, ул. Дубининская, д. 94 (Головной офис)
+                </address>
+
+                <div class="address-item__metro-stations">
+                    <span class="address-item__metro-station main-text">Павелецкая</span>
+                    <span class="address-item__metro-station main-text">Тульская</span>
+                </div>
+
+                <p class="address-item__opened-time description-text">Понедельник - пятница 09:30-17:00</p>
+
+                <a href="tel:+7 (800) 505 04 76" class="address-item__phone description-text">+7 (800) 505 04 76 </a>
+            </div>
+
+            <div class="address-item">
+                <address class="address-item__value heading-4">
+                    г. Москва, ул. Дубининская, д. 94 (Головной офис)
+                </address>
+
+                <div class="address-item__metro-stations">
+                    <span class="address-item__metro-station main-text">Павелецкая</span>
+                    <span class="address-item__metro-station main-text">Тульская</span>
+                </div>
+
+                <p class="address-item__opened-time description-text">Понедельник - пятница 09:30-17:00</p>
+
+                <a href="tel:+7 (800) 505 04 76" class="address-item__phone description-text">+7 (800) 505 04 76 </a>
+            </div>
+
+            <div class="address-item">
+                <address class="address-item__value heading-4">
+                    г. Москва, ул. Дубининская, д. 94 (Головной офис)
+                </address>
+
+                <div class="address-item__metro-stations">
+                    <span class="address-item__metro-station main-text">Павелецкая</span>
+                    <span class="address-item__metro-station main-text">Тульская</span>
+                </div>
+
+                <p class="address-item__opened-time description-text">Понедельник - пятница 09:30-17:00</p>
+
+                <a href="tel:+7 (800) 505 04 76" class="address-item__phone description-text">+7 (800) 505 04 76 </a>
+            </div>
+
+            <div class="address-item">
+                <address class="address-item__value heading-4">
+                    г. Москва, ул. Дубининская, д. 94 (Головной офис)
+                </address>
+
+                <div class="address-item__metro-stations">
+                    <span class="address-item__metro-station main-text">Павелецкая</span>
+                    <span class="address-item__metro-station main-text">Тульская</span>
+                </div>
+
+                <p class="address-item__opened-time description-text">Понедельник - пятница 09:30-17:00</p>
+
+                <a href="tel:+7 (800) 505 04 76" class="address-item__phone description-text">+7 (800) 505 04 76 </a>
+            </div>
+        </div>
+
+        <div class="ask-question-form-wrapper">
+            <div class="messages-wrapper">
+                <div class="success-message">
+                    <img src="/images/icons/success.svg" alt="иконка с галочкой" class="success-icon">
+                    <p class="success-message__text description-text">Спасибо. Ваш вопрос отправлен на модерацию</p>
+                </div>
+
+                <div class="error-message"></div>
+            </div>
+
+            <form action="" method="POST">
+                <h2 class="ask-question__title">Задать вопрос</h2>
+
+                <input type="text"
+                       placeholder="Имя*"
+                       name="fname"
+                       autocomplete="name">
+
+                <input type="tel"
+                       name="phone"
+                       autocomplete="tel"
+                       placeholder="Телефон*">
+
+                <textarea name="question" id="user-question" placeholder="Ваш вопрос"></textarea>
+
+                <button id="submit-question-form" class="mint-btn big filled lightning">Отправить</button>
+            </form>
+        </div>
+    </div>
+</section>
+
+<section class="delivery-cases">
+    <div class="delivery-cases__item">
+        <p class="heading-3 delivery-item__title">Доставка по Московской области и регионам РФ</p>
+
+        <!-- Включаемая -->
+        <p class="delivery-item__description">
+            Точная стоимость доставки рассчитывается при оформлении заказа и зависит от местонахождения населённого пункта, а также веса и стоимости заказа.
+            <br><br>
+            При необходимости, Вы можете предварительно уточнить стоимость, связавшись с менеджером Банка по номеру телефона или почте.
+        </p>
+    </div>
+
+    <div class="delivery-cases__item">
+        <p class="heading-3 delivery-item__title">Самовывоз</p>
+
+        <!-- Включаемая -->
+        <p class="delivery-item__description">
+            Самовывоз заказа осуществляется бесплатно, при наличии выбранных Вами монет в пунктах самовывоза*.
+            <br><br>
+            В офисах Москвы принимаются карты оплаты, кроме инвестиционных монет РФ, иностранных государств и некоторых памятных монет РФ.
+        </p>
+
+        <!-- Включаемая -->
+        <p class="delivery-item__little-description">
+            *Информацию о наличии монет в пункте самовывоза, а также о стоимости доставки Вы можете уточнить у менеджера Банка по:
+        </p>
+    </div>
+</section>
+
+<?php require "footer.html";?>
