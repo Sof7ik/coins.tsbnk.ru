3bfb0c074dea8bc1731a2e306198815bd050aa53
Leonid <bychkov.l47@mail.ru>
2022-02-03 02:01:27 +0300

    added 404 page responsive

---
+++ 404/404.php
@@ -0,0 +1,12 @@
+<?php require("header.html"); ?>
+<link rel="stylesheet" href="/404/404.css">
+
+<div class="main-inner-wrapper-404">
+    <img src="/images/404.png" alt="404" class="avatar-404">
+
+    <p class="404-message heading-3">Страница не найдена</p>
+
+    <a href="#" class="to-catalog mint-btn filled mini">Каталог</a>
+</div>
+
+<?php require "footer.html";?>
