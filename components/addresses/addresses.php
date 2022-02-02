cf562e420b1290bd45c1295c02715c4043719d97
Leonid <bychkov.l47@mail.ru>
2022-02-03 02:09:01 +0300

    took components in the own folder

---
+++ components/addresses/addresses.php
@@ -0,0 +1,138 @@
+<link rel="stylesheet" href="/components/addresses/addresses.css">
+
+<div class="addresses-list">
+    <div class="address-item">
+        <address class="address-item__value heading-4">
+            г. Москва, ул. Дубининская, д. 94 (Головной офис)
+        </address>
+
+        <div class="address-item__metro-stations">
+            <span class="address-item__metro-station main-text">Павелецкая</span>
+            <span class="address-item__metro-station main-text">Тульская</span>
+        </div>
+
+        <p class="address-item__opened-time description-text">Понедельник - пятница 09:30-17:00</p>
+
+        <a href="tel:+7 (800) 505 04 76" class="address-item__phone description-text">+7 (800) 505 04 76 </a>
+    </div>
+
+    <div class="address-item">
+        <address class="address-item__value heading-4">
+            г. Москва, ул. Дубининская, д. 94 (Головной офис)
+        </address>
+
+        <div class="address-item__metro-stations">
+            <span class="address-item__metro-station main-text">Павелецкая</span>
+            <span class="address-item__metro-station main-text">Тульская</span>
+        </div>
+
+        <p class="address-item__opened-time description-text">Понедельник - пятница 09:30-17:00</p>
+
+        <a href="tel:+7 (800) 505 04 76" class="address-item__phone description-text">+7 (800) 505 04 76 </a>
+    </div>
+
+    <div class="address-item">
+        <address class="address-item__value heading-4">
+            г. Москва, ул. Дубининская, д. 94 (Головной офис)
+        </address>
+
+        <div class="address-item__metro-stations">
+            <span class="address-item__metro-station main-text">Павелецкая</span>
+            <span class="address-item__metro-station main-text">Тульская</span>
+        </div>
+
+        <p class="address-item__opened-time description-text">Понедельник - пятница 09:30-17:00</p>
+
+        <a href="tel:+7 (800) 505 04 76" class="address-item__phone description-text">+7 (800) 505 04 76 </a>
+    </div>
+
+    <div class="address-item">
+        <address class="address-item__value heading-4">
+            г. Москва, ул. Дубининская, д. 94 (Головной офис)
+        </address>
+
+        <div class="address-item__metro-stations">
+            <span class="address-item__metro-station main-text">Павелецкая</span>
+            <span class="address-item__metro-station main-text">Тульская</span>
+        </div>
+
+        <p class="address-item__opened-time description-text">Понедельник - пятница 09:30-17:00</p>
+
+        <a href="tel:+7 (800) 505 04 76" class="address-item__phone description-text">+7 (800) 505 04 76 </a>
+    </div>
+
+    <div class="address-item">
+        <address class="address-item__value heading-4">
+            г. Москва, ул. Дубининская, д. 94 (Головной офис)
+        </address>
+
+        <div class="address-item__metro-stations">
+            <span class="address-item__metro-station main-text">Павелецкая</span>
+            <span class="address-item__metro-station main-text">Тульская</span>
+        </div>
+
+        <p class="address-item__opened-time description-text">Понедельник - пятница 09:30-17:00</p>
+
+        <a href="tel:+7 (800) 505 04 76" class="address-item__phone description-text">+7 (800) 505 04 76 </a>
+    </div>
+
+    <div class="address-item">
+        <address class="address-item__value heading-4">
+            г. Москва, ул. Дубининская, д. 94 (Головной офис)
+        </address>
+
+        <div class="address-item__metro-stations">
+            <span class="address-item__metro-station main-text">Павелецкая</span>
+            <span class="address-item__metro-station main-text">Тульская</span>
+        </div>
+
+        <p class="address-item__opened-time description-text">Понедельник - пятница 09:30-17:00</p>
+
+        <a href="tel:+7 (800) 505 04 76" class="address-item__phone description-text">+7 (800) 505 04 76 </a>
+    </div>
+
+    <div class="address-item">
+        <address class="address-item__value heading-4">
+            г. Москва, ул. Дубининская, д. 94 (Головной офис)
+        </address>
+
+        <div class="address-item__metro-stations">
+            <span class="address-item__metro-station main-text">Павелецкая</span>
+            <span class="address-item__metro-station main-text">Тульская</span>
+        </div>
+
+        <p class="address-item__opened-time description-text">Понедельник - пятница 09:30-17:00</p>
+
+        <a href="tel:+7 (800) 505 04 76" class="address-item__phone description-text">+7 (800) 505 04 76 </a>
+    </div>
+
+    <div class="address-item">
+        <address class="address-item__value heading-4">
+            г. Москва, ул. Дубининская, д. 94 (Головной офис)
+        </address>
+
+        <div class="address-item__metro-stations">
+            <span class="address-item__metro-station main-text">Павелецкая</span>
+            <span class="address-item__metro-station main-text">Тульская</span>
+        </div>
+
+        <p class="address-item__opened-time description-text">Понедельник - пятница 09:30-17:00</p>
+
+        <a href="tel:+7 (800) 505 04 76" class="address-item__phone description-text">+7 (800) 505 04 76 </a>
+    </div>
+
+    <div class="address-item">
+        <address class="address-item__value heading-4">
+            г. Москва, ул. Дубининская, д. 94 (Головной офис)
+        </address>
+
+        <div class="address-item__metro-stations">
+            <span class="address-item__metro-station main-text">Павелецкая</span>
+            <span class="address-item__metro-station main-text">Тульская</span>
+        </div>
+
+        <p class="address-item__opened-time description-text">Понедельник - пятница 09:30-17:00</p>
+
+        <a href="tel:+7 (800) 505 04 76" class="address-item__phone description-text">+7 (800) 505 04 76 </a>
+    </div>
+</div>
---
+++ components/news-list/about-company-news/template.php
@@ -0,0 +1,33 @@
+<link rel="stylesheet" href="/components/news-list/about-company-news/style.css">
+
+<div class="news-list">
+    <a href="" class="news-item">
+        <span class="news-item__date description-text">02 ноября</span>
+
+        <p class="news-item__announce-text main-text">
+            Шесть памятных монет выпущенных банком России получили награды XV международного конкурса
+            памятных
+            монет “Монетное созвездие - 2021”
+        </p>
+    </a>
+
+    <a href="" class="news-item">
+        <span class="news-item__date description-text">02 ноября</span>
+
+        <p class="news-item__announce-text main-text">
+            Шесть памятных монет выпущенных банком России получили награды XV международного конкурса
+            памятных
+            монет “Монетное созвездие - 2021”
+        </p>
+    </a>
+
+    <a href="" class="news-item">
+        <span class="news-item__date description-text">02 ноября</span>
+
+        <p class="news-item__announce-text main-text">
+            Шесть памятных монет выпущенных банком России получили награды XV международного конкурса
+            памятных
+            монет “Монетное созвездие - 2021”
+        </p>
+    </a>
+</div>
