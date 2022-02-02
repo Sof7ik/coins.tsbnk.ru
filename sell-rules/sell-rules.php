dead221e6b6579ad0d3ce9658c62d2bd29ea3f1d
Leonid <bychkov.l47@mail.ru>
2022-02-03 02:13:33 +0300

    added sell rules page responsive

---
+++ sell-rules/sell-rules.php
@@ -0,0 +1,18 @@
+<?php require("header.html"); ?>
+<link rel="stylesheet" href="/styles/sell-rules.css">
+
+<?php require $_SERVER["DOCUMENT_ROOT"] . "/components/breadcrumbs/breadcrumbs-blue/breadcrumbs.html"; ?>
+<link rel="stylesheet" href="/components/breadcrumbs/breadcrumbs-blue/breadcrumbs.css">
+
+<div class="content-container">
+    <div class="container-758">
+        <h1 class="heading-1 sell-rules__title">Правила продажи</h1>
+
+        <div class="sell-rules__rules-text">
+            <? require $_SERVER["DOCUMENT_ROOT"]."/includes/sell-rules-text.php"; ?>
+        </div>
+    </div>
+</div>
+
+
+<?php require "footer.html";?>
