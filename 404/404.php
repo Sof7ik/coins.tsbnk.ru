0bc9ce9592be9e219757f853c4e2c676cea4ccf4
Leonid <bychkov.l47@mail.ru>
2022-02-04 17:09:54 +0300

    fixed pathes, fixed <main> padding-bottom

--- 404/404.php
+++ 404/404.php
@@ -1,12 +1,14 @@
-<?php require("header.html"); ?>
+<?php require $_SERVER["DOCUMENT_ROOT"]."/header.html"; ?>
 <link rel="stylesheet" href="/404/404.css">
 
-<div class="main-inner-wrapper-404">
-    <img src="/images/404.png" alt="404" class="avatar-404">
+<div class="content-container">
+    <div class="main-inner-wrapper-404">
+        <img src="/images/404.png" alt="404" class="avatar-404">
 
-    <p class="404-message heading-3">Страница не найдена</p>
+        <p class="404-message heading-3">Страница не найдена</p>
 
-    <a href="#" class="to-catalog mint-btn filled mini">Каталог</a>
+        <a href="#" class="to-catalog mint-btn filled mini">Каталог</a>
+    </div>
 </div>
 
-<?php require "footer.html";?>
+<?php require $_SERVER["DOCUMENT_ROOT"]."/footer.html";?>
