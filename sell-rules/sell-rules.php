1ee584a5977813de588dc99fe6b868ad9919b437
Leonid <bychkov.l47@mail.ru>
2022-02-07 14:35:22 +0300

    fixed pathes. Sell rules page full responsive

--- sell-rules/sell-rules.php
+++ sell-rules/sell-rules.php
@@ -1,5 +1,5 @@
-<?php require("header.html"); ?>
-<link rel="stylesheet" href="/styles/sell-rules.css">
+<?php require($_SERVER["DOCUMENT_ROOT"]."/header.html"); ?>
+<link rel="stylesheet" href="/sell-rules/sell-rules.css">
 
 <?php require $_SERVER["DOCUMENT_ROOT"] . "/components/breadcrumbs/breadcrumbs-blue/breadcrumbs.html"; ?>
 <link rel="stylesheet" href="/components/breadcrumbs/breadcrumbs-blue/breadcrumbs.css">
@@ -8,11 +8,11 @@
     <div class="container-758">
         <h1 class="heading-1 sell-rules__title">Правила продажи</h1>
 
-        <div class="sell-rules__rules-text">
+        <div class="sell-rules__rules-text main-text">
             <? require $_SERVER["DOCUMENT_ROOT"]."/includes/sell-rules-text.php"; ?>
         </div>
     </div>
 </div>
 
 
-<?php require "footer.html";?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/footer.html");?>
