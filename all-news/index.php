cbf6f6fd39e6893e426081fb6a707eb8a31be5e7
Leonid <bychkov.l47@mail.ru>
2022-02-09 05:28:16 +0300

    full responsive all news page

---
+++ all-news/index.php
@@ -0,0 +1,10 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/header.html"); ?>
+<link rel="stylesheet" href="/all-news/all-news.css">
+
+<? require $_SERVER["DOCUMENT_ROOT"]."/components/breadcrumbs/breadcrumbs-gold/breadcrumbs.html"; ?>
+
+<div class="content-container">
+    <?php require $_SERVER["DOCUMENT_ROOT"]."/components/news/all-news-page/template.php"?>
+</div>
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/footer.html");?>
--- components/news/all-news-page/template.php
+++ components/news/all-news-page/template.php
@@ -1,3 +1,133 @@
-<?php
+<link rel="stylesheet" href="/components/news/all-news-page/style.css">
 
-?>
+<h1 class="heading-1 all-news__title">Новости</h1>
+
+<section class="all-news-items">
+    <a class="news-item" href="/news-detail/index.php">
+        <!-- <div class="news-item__date-name-wrapper"> -->
+            <p class="description-text news-item__date">21.10.2021</p>
+
+            <p class="heading-3 news-item__preview-text">
+                Шесть памятных монет выпущенных банком России получили награды XV международного конкурса памятных монет “Монетное созвездие - 2021”
+            </p>
+        <!-- </div> -->
+
+        <div class="preview-image-wrapper">
+            <img src="/images/encyclopedia/detail.jpg" alt="" class="news-item__preview-picture">
+        </div>
+
+        <p class="heading-4 watch-more-btn">Подробнее</p>
+    </a>
+
+    <a class="news-item" href="/news-detail/index.php">
+        <!-- <div class="news-item__date-name-wrapper"> -->
+            <p class="description-text news-item__date">21.10.2021</p>
+
+            <p class="heading-3 news-item__preview-text">
+                Шесть памятных монет выпущенных банком России получили награды XV международного конкурса памятных монет “Монетное созвездие - 2021”
+            </p>
+        <!-- </div> -->
+
+        <div class="preview-image-wrapper">
+            <img src="/images/encyclopedia/detail.jpg" alt="" class="news-item__preview-picture">
+        </div>
+
+        <p class="heading-4 watch-more-btn">Подробнее</p>
+    </a>
+
+    <a class="news-item" href="/news-detail/index.php">
+        <!-- <div class="news-item__date-name-wrapper"> -->
+            <p class="description-text news-item__date">21.10.2021</p>
+
+            <p class="heading-3 news-item__preview-text">
+                Шесть памятных монет выпущенных банком России получили награды XV международного конкурса памятных монет “Монетное созвездие - 2021”
+            </p>
+        <!-- </div> -->
+
+        <div class="preview-image-wrapper">
+            <img src="/images/encyclopedia/detail.jpg" alt="" class="news-item__preview-picture">
+        </div>
+
+        <p class="heading-4 watch-more-btn">Подробнее</p>
+    </a>
+    <a class="news-item" href="/news-detail/index.php">
+        <!-- <div class="news-item__date-name-wrapper"> -->
+            <p class="description-text news-item__date">21.10.2021</p>
+
+            <p class="heading-3 news-item__preview-text">
+                Шесть памятных монет выпущенных банком России получили награды XV международного конкурса памятных монет “Монетное созвездие - 2021”
+            </p>
+        <!-- </div> -->
+
+        <div class="preview-image-wrapper">
+            <img src="/images/encyclopedia/detail.jpg" alt="" class="news-item__preview-picture">
+        </div>
+
+        <p class="heading-4 watch-more-btn">Подробнее</p>
+    </a>
+    <a class="news-item" href="/news-detail/index.php">
+        <!-- <div class="news-item__date-name-wrapper"> -->
+            <p class="description-text news-item__date">21.10.2021</p>
+
+            <p class="heading-3 news-item__preview-text">
+                Шесть памятных монет выпущенных банком России получили награды XV международного конкурса памятных монет “Монетное созвездие - 2021”
+            </p>
+        <!-- </div> -->
+
+        <div class="preview-image-wrapper">
+            <img src="/images/encyclopedia/detail.jpg" alt="" class="news-item__preview-picture">
+        </div>
+
+        <p class="heading-4 watch-more-btn">Подробнее</p>
+    </a>
+
+    <a class="news-item" href="/news-detail/index.php">
+        <!-- <div class="news-item__date-name-wrapper"> -->
+            <p class="description-text news-item__date">21.10.2021</p>
+
+            <p class="heading-3 news-item__preview-text">
+                Шесть памятных монет выпущенных банком России получили награды XV международного конкурса памятных монет “Монетное созвездие - 2021”
+            </p>
+        <!-- </div> -->
+
+        <div class="preview-image-wrapper">
+            <img src="/images/encyclopedia/detail.jpg" alt="" class="news-item__preview-picture">
+        </div>
+
+        <p class="heading-4 watch-more-btn">Подробнее</p>
+    </a>
+    <a class="news-item" href="/news-detail/index.php">
+        <!-- <div class="news-item__date-name-wrapper"> -->
+            <p class="description-text news-item__date">21.10.2021</p>
+
+            <p class="heading-3 news-item__preview-text">
+                Шесть памятных монет выпущенных банком России получили награды XV международного конкурса памятных монет “Монетное созвездие - 2021”
+            </p>
+        <!-- </div> -->
+
+        <div class="preview-image-wrapper">
+            <img src="/images/encyclopedia/detail.jpg" alt="" class="news-item__preview-picture">
+        </div>
+
+        <p class="heading-4 watch-more-btn">Подробнее</p>
+    </a>
+    <a class="news-item" href="/news-detail/index.php">
+        <!-- <div class="news-item__date-name-wrapper"> -->
+            <p class="description-text news-item__date">21.10.2021</p>
+
+            <p class="heading-3 news-item__preview-text">
+                Шесть памятных монет выпущенных банком России получили награды XV международного конкурса памятных монет “Монетное созвездие - 2021”
+            </p>
+        <!-- </div> -->
+
+        <div class="preview-image-wrapper">
+            <img src="/images/encyclopedia/detail.jpg" alt="" class="news-item__preview-picture">
+        </div>
+
+        <p class="heading-4 watch-more-btn">Подробнее</p>
+    </a>
+
+
+</section>
+
+ <? require $_SERVER["DOCUMENT_ROOT"] ."/components/pagination/template.php"; ?>
