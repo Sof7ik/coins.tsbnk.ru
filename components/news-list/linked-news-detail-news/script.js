853b32b5073372597fd069ef49c69bfa88228d2e
Leonid <bychkov.l47@mail.ru>
2022-02-13 17:37:40 +0300

    detail news page

---
+++ components/news-list/linked-news-detail-news/script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", e => {
+    const swiper = new Swiper('.linked-detail-news-slider', {
+        navigation: {
+            nextEl: ".slider-nav .swiper-button-next",
+            prevEl: ".slider-nav .swiper-button-prev"
+        },
+        breakpoints: {
+            1400: {
+                spaceBetween: 100,
+                slidesPerView: 3,
+            },
+            1200: {
+                spaceBetween: 57,
+                slidesPerView: 3,
+            },
+            992: {
+                spaceBetween: 40,
+                slidesPerView: 3,
+            },
+            // 768: {
+            //     spaceBetween: 20,
+            //     slidesPerView: 2,
+            // },
+            576: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            0: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            }
+        }
+    });
+})
---
+++ components/news-list/linked-news-detail-news/template.php
@@ -0,0 +1,59 @@
+<link rel="stylesheet" href="/components/news-list/linked-news-detail-news/style.css">
+<script src="/components/news-list/linked-news-detail-news/script.js" defer></script>
+
+<section class="linked-detail-news">
+    <div class="content-container">
+        <div class="title-slider-nav-wrapper">
+            <h2 class="heading-3 linked-detail-news__title">Другие новости</h2>
+    
+            <div class="slider-nav">
+                <button class="swiper-button-prev">
+                    <img src="/images/icons/slider-arrow-about-company.svg" alt="стрлека влево">
+                </button>
+                <button class="swiper-button-next">
+                    <img src="/images/icons/slider-arrow-about-company.svg" alt="стрлека вправо">
+                </button>
+            </div>
+        </div>
+    
+        <div class="swiper linked-detail-news-slider">
+            <div class="swiper-wrapper">
+                <a class="swiper-slide linked-news-item" href="/news-detail/news-detail.php">
+                    <p class="linked-news-item__date description-text">21.10.2021</p>
+    
+                    <p class="heading-4 linked-news-item__name">
+                        Королевский монетный двор Великобритании отчеканил золотые инвестиционные монеты “THE GOLD STANDARD”
+                    </p>
+    
+                    <img src="/images/encyclopedia/detail.jpg" alt="" class="linked-news-item__preview-image">
+    
+                    <p class="description-text watch-more">Подробнее</p>
+                </a>
+    
+                <a class="swiper-slide linked-news-item" href="/news-detail/news-detail.php">
+                    <p class="linked-news-item__date description-text">21.10.2021</p>
+    
+                    <p class="heading-4 linked-news-item__name">
+                        Королевский монетный двор Великобритании отчеканил золотые инвестиционные монеты “THE GOLD STANDARD”
+                    </p>
+    
+                    <img src="/images/encyclopedia/preview-1.jpg" alt="" class="linked-news-item__preview-image">
+    
+                    <p class="description-text watch-more">Подробнее</p>
+                </a>
+
+                <a class="swiper-slide linked-news-item" href="/news-detail/news-detail.php">
+                    <p class="linked-news-item__date description-text">21.10.2021</p>
+    
+                    <p class="heading-4 linked-news-item__name">
+                        Королевский монетный двор Великобритании отчеканил золотые инвестиционные монеты “THE GOLD STANDARD”
+                    </p>
+    
+                    <img src="/images/encyclopedia/preview.jpg" alt="" class="linked-news-item__preview-image">
+    
+                    <p class="description-text watch-more">Подробнее</p>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
