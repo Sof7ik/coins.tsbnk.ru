d7c639ec39df30d4419ca68491dcb33651e79d66
Leonid <bychkov.l47@mail.ru>
2022-02-13 17:39:59 +0300

    fixed mobile images

--- components/news/all-news-page/template.php
+++ components/news/all-news-page/template.php
@@ -3,7 +3,7 @@
 <h1 class="heading-1 all-news__title">Новости</h1>
 
 <section class="all-news-items">
-    <a class="news-item" href="/news-detail/index.php">
+    <a class="news-item" href="/news-detail/news-detail.php">
         <!-- <div class="news-item__date-name-wrapper"> -->
             <p class="description-text news-item__date">21.10.2021</p>
 
@@ -19,7 +19,7 @@
         <p class="heading-4 watch-more-btn">Подробнее</p>
     </a>
 
-    <a class="news-item" href="/news-detail/index.php">
+    <a class="news-item" href="/news-detail/news-detail.php">
         <!-- <div class="news-item__date-name-wrapper"> -->
             <p class="description-text news-item__date">21.10.2021</p>
 
@@ -35,37 +35,7 @@
         <p class="heading-4 watch-more-btn">Подробнее</p>
     </a>
 
-    <a class="news-item" href="/news-detail/index.php">
-        <!-- <div class="news-item__date-name-wrapper"> -->
-            <p class="description-text news-item__date">21.10.2021</p>
-
-            <p class="heading-3 news-item__preview-text">
-                Шесть памятных монет выпущенных банком России получили награды XV международного конкурса памятных монет “Монетное созвездие - 2021”
-            </p>
-        <!-- </div> -->
-
-        <div class="preview-image-wrapper">
-            <img src="/images/encyclopedia/detail.jpg" alt="" class="news-item__preview-picture">
-        </div>
-
-        <p class="heading-4 watch-more-btn">Подробнее</p>
-    </a>
-    <a class="news-item" href="/news-detail/index.php">
-        <!-- <div class="news-item__date-name-wrapper"> -->
-            <p class="description-text news-item__date">21.10.2021</p>
-
-            <p class="heading-3 news-item__preview-text">
-                Шесть памятных монет выпущенных банком России получили награды XV международного конкурса памятных монет “Монетное созвездие - 2021”
-            </p>
-        <!-- </div> -->
-
-        <div class="preview-image-wrapper">
-            <img src="/images/encyclopedia/detail.jpg" alt="" class="news-item__preview-picture">
-        </div>
-
-        <p class="heading-4 watch-more-btn">Подробнее</p>
-    </a>
-    <a class="news-item" href="/news-detail/index.php">
+    <a class="news-item" href="/news-detail/news-detail.php">
         <!-- <div class="news-item__date-name-wrapper"> -->
             <p class="description-text news-item__date">21.10.2021</p>
 
@@ -81,37 +51,7 @@
         <p class="heading-4 watch-more-btn">Подробнее</p>
     </a>
 
-    <a class="news-item" href="/news-detail/index.php">
-        <!-- <div class="news-item__date-name-wrapper"> -->
-            <p class="description-text news-item__date">21.10.2021</p>
-
-            <p class="heading-3 news-item__preview-text">
-                Шесть памятных монет выпущенных банком России получили награды XV международного конкурса памятных монет “Монетное созвездие - 2021”
-            </p>
-        <!-- </div> -->
-
-        <div class="preview-image-wrapper">
-            <img src="/images/encyclopedia/detail.jpg" alt="" class="news-item__preview-picture">
-        </div>
-
-        <p class="heading-4 watch-more-btn">Подробнее</p>
-    </a>
-    <a class="news-item" href="/news-detail/index.php">
-        <!-- <div class="news-item__date-name-wrapper"> -->
-            <p class="description-text news-item__date">21.10.2021</p>
-
-            <p class="heading-3 news-item__preview-text">
-                Шесть памятных монет выпущенных банком России получили награды XV международного конкурса памятных монет “Монетное созвездие - 2021”
-            </p>
-        <!-- </div> -->
-
-        <div class="preview-image-wrapper">
-            <img src="/images/encyclopedia/detail.jpg" alt="" class="news-item__preview-picture">
-        </div>
-
-        <p class="heading-4 watch-more-btn">Подробнее</p>
-    </a>
-    <a class="news-item" href="/news-detail/index.php">
+    <a class="news-item" href="/news-detail/news-detail.php">
         <!-- <div class="news-item__date-name-wrapper"> -->
             <p class="description-text news-item__date">21.10.2021</p>
 
@@ -127,7 +67,69 @@
         <p class="heading-4 watch-more-btn">Подробнее</p>
     </a>
 
+    <a class="news-item" href="/news-detail/news-detail.php">
+        <!-- <div class="news-item__date-name-wrapper"> -->
+            <p class="description-text news-item__date">21.10.2021</p>
 
+            <p class="heading-3 news-item__preview-text">
+                Шесть памятных монет выпущенных банком России получили награды XV международного конкурса памятных монет “Монетное созвездие - 2021”
+            </p>
+        <!-- </div> -->
+
+        <div class="preview-image-wrapper">
+            <img src="/images/encyclopedia/detail.jpg" alt="" class="news-item__preview-picture">
+        </div>
+
+        <p class="heading-4 watch-more-btn">Подробнее</p>
+    </a>
+
+    <a class="news-item" href="/news-detail/news-detail.php">
+        <!-- <div class="news-item__date-name-wrapper"> -->
+            <p class="description-text news-item__date">21.10.2021</p>
+
+            <p class="heading-3 news-item__preview-text">
+                Шесть памятных монет выпущенных банком России получили награды XV международного конкурса памятных монет “Монетное созвездие - 2021”
+            </p>
+        <!-- </div> -->
+
+        <div class="preview-image-wrapper">
+            <img src="/images/encyclopedia/detail.jpg" alt="" class="news-item__preview-picture">
+        </div>
+
+        <p class="heading-4 watch-more-btn">Подробнее</p>
+    </a>
+    
+    <a class="news-item" href="/news-detail/news-detail.php">
+        <!-- <div class="news-item__date-name-wrapper"> -->
+            <p class="description-text news-item__date">21.10.2021</p>
+
+            <p class="heading-3 news-item__preview-text">
+                Шесть памятных монет выпущенных банком России получили награды XV международного конкурса памятных монет “Монетное созвездие - 2021”
+            </p>
+        <!-- </div> -->
+
+        <div class="preview-image-wrapper">
+            <img src="/images/encyclopedia/detail.jpg" alt="" class="news-item__preview-picture">
+        </div>
+
+        <p class="heading-4 watch-more-btn">Подробнее</p>
+    </a>
+
+    <a class="news-item" href="/news-detail/news-detail.php">
+        <!-- <div class="news-item__date-name-wrapper"> -->
+            <p class="description-text news-item__date">21.10.2021</p>
+
+            <p class="heading-3 news-item__preview-text">
+                Шесть памятных монет выпущенных банком России получили награды XV международного конкурса памятных монет “Монетное созвездие - 2021”
+            </p>
+        <!-- </div> -->
+
+        <div class="preview-image-wrapper">
+            <img src="/images/encyclopedia/detail.jpg" alt="" class="news-item__preview-picture">
+        </div>
+
+        <p class="heading-4 watch-more-btn">Подробнее</p>
+    </a>
 </section>
 
  <? require $_SERVER["DOCUMENT_ROOT"] ."/components/pagination/template.php"; ?>
