cc3c23cf3e4e9cc45c3c75d195aee99efc4c541b
Sof7ik <alekse-bychkov@rambler.ru>
2022-02-20 22:21:51 +0300

    added mainpage

---
+++ components/news-list/mainpage-buyout-coins/template.php
@@ -0,0 +1,79 @@
+<link rel="stylesheet" href="/components/">
+
+<section class="buyout-coins">
+    <div class="buyout-coins__title-navigation-wrapper">
+        <h2 class="heading-3 buyout-coins__title">Выкуп монет</h2>
+
+        <div class="mobile-buyout-coins-slider-nav">
+            <button class="swiper-button-prev">
+                <img src="/images/icons/slider-arrow-about-company.svg" alt="стрелка влево">
+            </button>
+
+            <button class="swiper-button-next">
+                <img src="/images/icons/slider-arrow-about-company.svg" alt="стрелка влево">
+            </button>
+        </div>
+    </div>
+
+    <div class="swiper buyout-coins-slider">
+        <div class="swiper-wrapper">
+            <a class="swiper-slide buyout-coin-item" href="/catalog/catalog.php">
+                <div class="buyout-coin__left-wrapper">
+                    <p class="buyout-coin-name">
+                        Золотая инвестиционная российская монета Георгий Победоносец 50 рублей
+                    </p>
+
+                    <p class="buyout-coin__more-info">Подробнее</p>
+                </div>
+
+                <img src="/images/coins/coin-2.png" alt="" class="buyout-coin__image">
+            </a>
+
+            <a class="swiper-slide buyout-coin-item" href="/catalog/catalog.php">
+                <div class="buyout-coin__left-wrapper">
+                    <p class="buyout-coin-name">
+                        Монета Золотой червонец (сеятель). Вес 7.74 Проба 900
+                    </p>
+
+                    <p class="buyout-coin__more-info">Подробнее</p>
+                </div>
+
+                <img src="/images/coins/coin-2.png" alt="" class="buyout-coin__image">
+            </a>
+
+            <a class="swiper-slide buyout-coin-item" href="/catalog/catalog.php">
+                <div class="buyout-coin__left-wrapper">
+                    <p class="buyout-coin-name">
+                        Золотая инвестиционная российская монета Георгий Победоносец 50 рублей
+                    </p>
+
+                    <p class="buyout-coin__more-info">Подробнее</p>
+                </div>
+
+                <img src="/images/coins/coin-2.png" alt="" class="buyout-coin__image">
+            </a>
+
+            <a class="swiper-slide buyout-coin-item" href="/catalog/catalog.php">
+                <div class="buyout-coin__left-wrapper">
+                    <p class="buyout-coin-name">
+                        Монета Золотой червонец (сеятель). Вес 7.74 Проба 900
+                    </p>
+
+                    <p class="buyout-coin__more-info">Подробнее</p>
+                </div>
+
+                <img src="/images/coins/coin-2.png" alt="" class="buyout-coin__image">
+            </a>
+        </div>
+
+        <div class="desktop-buyout-coins-nav-wrapper">
+            <button class="swiper-button-prev">
+                <img src="/images/icons/linked-coins/slider-arrow.svg" alt="стрелка влево">    
+            <button>
+
+            <button class="swiper-button-next">
+                <img src="/images/icons/linked-coins/slider-arrow.svg" alt="стрелка влево">    
+            </button>
+        </div>
+    </div>
+</section>
---
+++ index.php
@@ -0,0 +1,100 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/header.html"); ?>
+<link rel="stylesheet" href="/styles/mainpage.css">
+
+<div class="content-container">
+    <section class="famous-collections">
+        <div class="famous-collections__title-wrapper">
+            <h2 class="heading-3">Знаменитые коллекции</h2>
+            
+            <a href="/all-collections/all-collections.php" class="watch-all-collections">Посмотреть все</a>
+        </div>
+
+        <div class="collections-list">
+            <div class="collection-item">
+                <div class="collection-item__inner-wrapper">
+                    <div class="collection-name-type-wrapper">
+                        <h3 class="heading-4 collection__name">Защита дикой природы</h3>
+                        <p class="collection__type">Коллекция монет</p>
+                    </div>
+    
+                    <div class="collection__preview-images">
+                        <img src="/images/collections/collection-coin.png" alt="" class="collection__preview-image">
+                        <img src="/images/collections/collection-coin.png" alt="" class="collection__preview-image">
+                        <img src="/images/collections/collection-coin.png" alt="" class="collection__preview-image">
+                    </div>
+    
+                    <a href="/catalog/catalog.php" class="rotating-image-wrapper">
+                        <img src="/images/rotating-text/white/rotating-text.svg" alt="смотреть коллекцию" class="rotating-image">
+                        <img src="/images/rotating-text/white/rotating-text-inner-arrow.svg" alt="стрелка" class="inner-arrow">
+                    </a>
+                </div>
+            </div>
+
+            <div class="collection-item">
+                <div class="collection-item__inner-wrapper">
+                    <div class="collection-name-type-wrapper">
+                        <h3 class="heading-4 collection__name">Искусство Тиффани</h3>
+                        <p class="collection__type">Коллекция монет</p>
+                    </div>
+    
+                    <div class="collection__preview-images">
+                        <img src="/images/collections/collection-coin.png" alt="" class="collection__preview-image">
+                        <img src="/images/collections/collection-coin.png" alt="" class="collection__preview-image">
+                        <img src="/images/collections/collection-coin.png" alt="" class="collection__preview-image">
+                    </div>
+    
+                    <a href="/catalog/catalog.php" class="rotating-image-wrapper">
+                        <img src="/images/rotating-text/white/rotating-text.svg" alt="смотреть коллекцию" class="rotating-image">
+                        <img src="/images/rotating-text/white/rotating-text-inner-arrow.svg" alt="стрелка" class="inner-arrow">
+                    </a>
+                </div>
+            </div>
+
+            <div class="collection-item">
+                <div class="collection-item__inner-wrapper">
+                    <div class="collection-name-type-wrapper">
+                        <h3 class="heading-4 collection__name">Православные монеты</h3>
+                        <p class="collection__type">Коллекция монет</p>
+                    </div>
+    
+                    <div class="collection__preview-images">
+                        <img src="/images/collections/collection-coin.png" alt="" class="collection__preview-image">
+                        <img src="/images/collections/collection-coin.png" alt="" class="collection__preview-image">
+                        <img src="/images/collections/collection-coin.png" alt="" class="collection__preview-image">
+                    </div>
+    
+                    <a href="/catalog/catalog.php" class="rotating-image-wrapper">
+                        <img src="/images/rotating-text/white/rotating-text.svg" alt="смотреть коллекцию" class="rotating-image">
+                        <img src="/images/rotating-text/white/rotating-text-inner-arrow.svg" alt="стрелка" class="inner-arrow">
+                    </a>
+                </div>
+            </div>
+
+            <div class="collection-item">
+                <div class="collection-item__inner-wrapper">
+                    <div class="collection-name-type-wrapper">
+                        <h3 class="heading-4 collection__name">Метеориты</h3>
+                        <p class="collection__type">Коллекция монет</p>
+                    </div>
+    
+                    <div class="collection__preview-images">
+                        <img src="/images/collections/collection-coin.png" alt="" class="collection__preview-image">
+                        <img src="/images/collections/collection-coin.png" alt="" class="collection__preview-image">
+                        <img src="/images/collections/collection-coin.png" alt="" class="collection__preview-image">
+                    </div>
+    
+                    <a href="/catalog/catalog.php" class="rotating-image-wrapper">
+                        <img src="/images/rotating-text/white/rotating-text.svg" alt="смотреть коллекцию" class="rotating-image">
+                        <img src="/images/rotating-text/white/rotating-text-inner-arrow.svg" alt="стрелка" class="inner-arrow">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?
+        require $_SERVER["DOCUMENT_ROOT"] . "/components/news-list/mainpage-buyout-coins/template.php";
+    ?>
+</div>
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/footer.html");?>
