ed7bd848cc042ac0257924a18f165c29bf1320cd
Leonid Bychkov <bychkov.l47@mail.ru>
2022-02-21 04:12:05 +0300

    fix dark header font color, added cart block fucntionality

--- js/header.js
+++ js/header.js
@@ -1,23 +1,19 @@
-function openShopMenu(event)
-{
+function openShopMenu(event) {
     const target = event.currentTarget;
 
     const shopMenuWrapper = document.querySelector(".shop-menu-wrapper");
 
-    if (target.classList.contains('opened'))
-    {
+    if (target.classList.contains('opened')) {
         shopMenuWrapper.classList.add("hidden");
     }
-    else
-    {
+    else {
         shopMenuWrapper.classList.remove("hidden");
     }
 
     target.classList.toggle("opened");
 }
 
-function onChangeInput(event)
-{
+function onChangeInput(event) {
     const query = event.currentTarget.value;
 
     const searchWrapper = document.querySelector('.header-search-wrapper');
@@ -30,8 +26,7 @@ function onChangeInput(event)
     })
 }
 
-function onInputBlur(event)
-{
+function onInputBlur(event) {
     console.log("blur");
 
     const searchWrapper = document.querySelector('header .header-search-wrapper');
@@ -40,47 +35,38 @@ function onInputBlur(event)
     const headerUp = document.querySelector("#page-header .header-up");
     headerUp.classList.remove("search-opened");
 
-    if (event === null)
-    {
+    if (event === null) {
         searchWrapper.querySelector('input').blur();
     }
 }
 
-function onInputKeydown(event)
-{
-    if(event.key === "Escape")
-    {
+function onInputKeydown(event) {
+    if (event.key === "Escape") {
         onInputBlur(null);
     }
-    else if (event.key === "Enter")
-    {
-        if (event.currentTarget.value.trim() !== "")
-        {
-            window.location=`/all-search-results/?query=${event.currentTarget.value}`;
+    else if (event.key === "Enter") {
+        if (event.currentTarget.value.trim() !== "") {
+            window.location = `/all-search-results/?query=${event.currentTarget.value}`;
         }
     }
 }
 
-function onInputFocus(event)
-{
+function onInputFocus(event) {
     const searchWrapper = document.querySelector('header .header-search-wrapper');
     searchWrapper.classList.add('active');
 }
 
-function onPageScroll(event)
-{
+function onPageScroll(event) {
     const header = document.querySelector("header#page-header");
     const headerUp = header.querySelector(".header-up");
 
-    if (document.body.scrollTop > headerUp.clientHeight)
-    {
+    if (document.body.scrollTop > headerUp.clientHeight) {
         header.classList.add('sticky');
         document.querySelector('.header-mrb').style.display = "block";
 
         // document.querySelector('#page-header .header-bottom .shop-menu-wrapper').style.height = `51px`;
     }
-    else
-    {
+    else {
         header.classList.remove('sticky');
         document.querySelector('.header-mrb').style.display = "none";
 
@@ -88,36 +74,58 @@ function onPageScroll(event)
     }
 }
 
-function openStickyHeaderSearch(event)
-{
+function openStickyHeaderSearch(event) {
     console.log("open f");
-    
+
     const headerUp = document.querySelector("#page-header .header-up");
     headerUp.classList.add("search-opened");
 
     const input = document.getElementById('header-search-input');
-    
+
     input.focus();
 }
 
-function openMobileCatalog(event)
-{
+function openMobileCatalog(event) {
     console.log("хуйня")
 
     const target = event.currentTarget;
     const mobileCatalogMenu = target.closest(".mobile-catalog-wrapper")
-                                .querySelector(".mobile-catalog-navigation-wrapper");
+        .querySelector(".mobile-catalog-navigation-wrapper");
 
-    if (target.classList.contains("opened"))
-    {
+    if (target.classList.contains("opened")) {
         mobileCatalogMenu.classList.add("hidden");
     }
-    else
-    {
+    else {
         mobileCatalogMenu.classList.remove("hidden");
     }
 
     target.classList.toggle("opened");
+}
+
+// cart block
+function closeHeaderCartByEsc(event) {
+    if (event.key === "Escape") {
+        closeHeaderCart(null);
+    }
+}
+
+function openHeaderCart(event) {
+    const target = event.currentTarget;
+
+    const parentWrapper = target.closest(".cart-wrapper");
+
+    parentWrapper.classList.add("opened");
+
+    document.addEventListener('keydown', closeHeaderCartByEsc);
+
+}
+
+function closeHeaderCart(event) {
+    const wrapper = document.getElementById("open-header-cart").closest(".cart-wrapper");
+
+    wrapper.classList.remove("opened");
+
+    document.removeEventListener("keydown", closeHeaderCartByEsc);
 }
 
 document.addEventListener("DOMContentLoaded", e => {
@@ -141,7 +149,7 @@ document.addEventListener("DOMContentLoaded", e => {
     elems.forEach(item => {
         item.addEventListener('click', onInputBlur);
     })
-    
+
     document.getElementById('header-search-input').addEventListener('input', onChangeInput);
     document.getElementById('header-search-input').addEventListener("keydown", onInputKeydown);
 
@@ -150,4 +158,8 @@ document.addEventListener("DOMContentLoaded", e => {
     document.getElementById('open-sticky-search').addEventListener('click', openStickyHeaderSearch);
 
     document.getElementById("open-mobile-catalog").addEventListener("click", openMobileCatalog);
+
+    // cart
+    document.getElementById("open-header-cart").addEventListener('click', openHeaderCart);
+    document.querySelector(".personal-navigation__item.cart-wrapper .header-cart-fixed-bg").addEventListener('click', closeHeaderCart);
 })
