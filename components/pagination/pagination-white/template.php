c1b9f6ac49a7c0344f2320dff7e807ddaa0f35e2
Sof7ik <alekse-bychkov@rambler.ru>
2022-02-24 18:25:59 +0300

    added white pagination component, added svg instead of <img>

---
+++ components/pagination/pagination-white/template.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href="/components/pagination/pagination-white/style.css">
+
+<div class="bottom-page-pagination">
+    <a href="#" class="pagination-arrow-wrapper disabled">
+        <svg class="pagination-arrow left" width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12.9015 8.03349L5.03854 0.192242C4.78092 -0.064517 4.36382 -0.0640854 4.10663 0.19357C3.84964 0.451192 3.85031 0.86852 4.10796 1.12548L11.5029 8.50003L4.10769 15.8745C3.85007 16.1315 3.84941 16.5486 4.10637 16.8063C4.23529 16.9354 4.4042 17 4.5731 17C4.74157 17 4.90981 16.9358 5.03851 16.8076L12.9015 8.96653C13.0256 8.84308 13.0952 8.67508 13.0952 8.50003C13.0952 8.32498 13.0254 8.15718 12.9015 8.03349Z"/>
+        </svg>
+    </a>
+
+    <nav class="pagination-numbers">
+        <a href="" class="pagination-number">1</a>
+        <a href="" class="pagination-number active">2</a>
+        <a href="" class="pagination-number">3</a>
+        <a href="" class="pagination-number">4</a>
+        <a href="" class="pagination-number">5</a>
+    </nav>
+
+    <a href="#" class="pagination-arrow-wrapper">
+        <svg class="pagination-arrow" width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12.9015 8.03349L5.03854 0.192242C4.78092 -0.064517 4.36382 -0.0640854 4.10663 0.19357C3.84964 0.451192 3.85031 0.86852 4.10796 1.12548L11.5029 8.50003L4.10769 15.8745C3.85007 16.1315 3.84941 16.5486 4.10637 16.8063C4.23529 16.9354 4.4042 17 4.5731 17C4.74157 17 4.90981 16.9358 5.03851 16.8076L12.9015 8.96653C13.0256 8.84308 13.0952 8.67508 13.0952 8.50003C13.0952 8.32498 13.0254 8.15718 12.9015 8.03349Z"/>
+        </svg>
+    </a>
+</div>
